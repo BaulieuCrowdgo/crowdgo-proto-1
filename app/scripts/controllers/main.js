4c3c97b845221035725f80e849bbf50776b3ad0d
BaulieuCrowdGo <baulieu.de-reboul@berkeley.edu>
2016-10-05 17:48:43 -0700

    creating angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crowdgoProto1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crowdgoProto1App
+ */
+angular.module('crowdgoProto1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
